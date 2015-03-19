30152a18a9df03ee3c89d0fae640e4e17b3c73d5
Rob Pilling <robpilling@gmail.com>
2015-03-19 23:02:40 +0000

    Tag die() with printf attribute

--- die.h
+++ die.h
@@ -1,6 +1,9 @@
 #ifndef DIE_H
 #define DIE_H
 
+#include "macros.h"
+
+attr_printf(1, 2)
 void die(const char *fmt, ...);
 
 #endif
--- macros.h
+++ macros.h
@@ -3,4 +3,8 @@
 
 #define countof(x) (sizeof(x) / sizeof*(x))
 
+#ifdef __GNUC__
+#  define attr_printf(x, y) __attribute__((format(printf, x, y)))
+#endif
+
 #endif
