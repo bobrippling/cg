88c98da0f02e2d97ff21e63ce024f5e0e9e18bc7
Rob Pilling <robpilling@gmail.com>
2017-04-20 22:30:30 +0100

    Initialise registers to NAME_NOWHERE and catch in codegen

--- location.h
+++ location.h
@@ -19,7 +19,7 @@ struct location
 		int off;
 	} u;
 };
-#define location_init_reg(l) ((l)->where = NAME_IN_REG, (l)->u.reg = regt_make_invalid())
+#define location_init_reg(l) ((l)->where = NAME_NOWHERE, (l)->u.reg = regt_make_invalid())
 unsigned location_hash(struct location const *);
 bool location_eq(struct location const *, struct location const *);
 
